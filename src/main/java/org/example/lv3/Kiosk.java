a0d9d284ab81d26ae51a661934420c4adba005ce
dbdnsdb123 <dbdnsdb123@naver.com>
2025-07-16 20:27:46 +0900

    [feat] lv3 add kiosk

--- src/main/java/org/example/lv3/Kiosk.java
+++ src/main/java/org/example/lv3/Kiosk.java
@@ -1,18 +1,18 @@
-package org.example.lv2;
+package org.example.lv3;
 
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
-public class Lv2Kiosk {
-    public static void main(String[] args) {
-        List<MenuItem> menuItems = new ArrayList<>();
-        Scanner sc = new Scanner(System.in);
+public class Kiosk {
+    private final List<MenuItem> menuItems;
 
-        menuItems.add(new MenuItem("ShackBurger", "6.9", "토마토, 양상추, 쉑소스가 토핑된 치즈버거"));
-        menuItems.add(new MenuItem("SmokeShack", "8.9", "베이컨, 체리 페퍼에 쉑소스가 토핑된 치즈버거"));
-        menuItems.add(new MenuItem("Cheeseburger", "6.9", "포테이토 번과 비프패티, 치즈가 토핑된 치즈버거"));
-        menuItems.add(new MenuItem("Hamburger", "5.4", "비프패티를 기반으로 야채가 들어간 기본버거"));
+    public Kiosk(List<MenuItem> menuItems){
+        this.menuItems = menuItems;
+    }
+
+    public void start(){
+        Scanner sc = new Scanner(System.in);
 
         while (true) {
             System.out.println("[ SHAKESHACK MENU ]");
@@ -35,4 +35,3 @@ public class Lv2Kiosk {
         }
     }
 }
-
---
+++ src/main/java/org/example/lv3/MenuItem.java
@@ -0,0 +1,23 @@
+package org.example.lv3;
+
+public class MenuItem {
+    private final String name, price, description;
+
+    public MenuItem(String name, String price, String description) {
+        this.name = name;
+        this.price = price;
+        this.description = description;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getPrice() {
+        return price;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+}
