90906c19ee94c86542ecc38055f9737cbc5d00e1
dbdnsdb123 <dbdnsdb123@naver.com>
2025-07-18 20:47:38 +0900

    [feat] fix error

--- src/main/java/org/example/lv4/Kiosk.java
+++ src/main/java/org/example/lv4/Kiosk.java
@@ -28,9 +28,15 @@ public class Kiosk {
             int num1 = sc.nextInt();
             sc.nextLine(); //버퍼 초기화
 
+            if (num1 == 0) {
+                System.out.println("프로그램을 종료합니다.");
+                sc.close();
+                return;
+            }
+
             while (true) {
+                List<MenuItem> menuItems = menu.get(num1-1).getMenuItems();
                 if (num1 == 1) {
-                    List<MenuItem> menuItems = menu.get(num1-1).getMenuItems();
                     // Menu가 가진 List<MenuItem>을 반복문을 활용하여 햄버거 메뉴 출력
                     System.out.println("[ BURGERS MENU ]");
                     int cnt = 0;
@@ -44,7 +50,6 @@ public class Kiosk {
                     // 입력 받은 숫자가 올바르다면 인덱스로 활용해서 Menu가 가지고 있는 List<MenuItem>에 접근하기
                     if (num2 > 0 && num2 < 5) {
                         // menu.getMenuItems().get(i); 같은 형식으로 하나씩 들어가서 얻어와야 합니다.
-
                         MenuItem category = menuItems.get(num2 - 1);
                         System.out.printf("선택한 메뉴: %s | W%s | %s \n", category.getName(), category.getPrice(), category.getDescription());
                     } else if (num2 == 0) {
@@ -52,11 +57,7 @@ public class Kiosk {
                     } else {
                         throw new IllegalArgumentException("오류 : 0~4번 숫자를 입력해주세요.");
                     }
-                } else if (num1 == 0) {
-                    System.out.println("프로그램을 종료합니다.");
-                    sc.close();
-                    return;
-                } else if (num1 == 2 || num1 == 3) {
+                }else if (num1 == 2 || num1 == 3) {
                     System.out.println("현재 판매하지 않는 품목입니다.");
                     break;
                 } else {
