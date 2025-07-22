3b0b38af0970567bf8274b5866c1d08062af5031
dbdnsdb123 <dbdnsdb123@naver.com>
2025-07-22 20:47:14 +0900

    [feat] add challenge lv2 comment

--- src/main/java/org/example/challenge/lv2/Lv2Kiosk.java
+++ src/main/java/org/example/challenge/lv2/Lv2Kiosk.java
@@ -1,9 +1,5 @@
 package org.example.challenge.lv2;
 
-import java.util.ArrayList;
-import java.util.List;
-
-
 // 기본값들을 지정하고 Kiosk를 실행하는 메인 클래스
 public class Lv2Kiosk {
     public static void main(String[] args) {
