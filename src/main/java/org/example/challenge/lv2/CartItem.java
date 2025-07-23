f31371e2acdbaf65fd84fb8ff9e90325fc6bb434
dbdnsdb123 <dbdnsdb123@naver.com>
2025-07-23 11:41:33 +0900

    [feat] add challenge lv2 MenuItem

--- src/main/java/org/example/challenge/lv2/CartItem.java
+++ src/main/java/org/example/challenge/lv2/CartItem.java
@@ -2,15 +2,11 @@ package org.example.challenge.lv2;
 
 import java.math.BigDecimal;
 
-// 주문한 항목의 이름, 가격, 설명
-public enum CartItem {
-    BURGER("ShackBurger","6.9","토마토, 양상추, 쉑소스가 토핑된 치즈버거"),
-    DRINKS("ShackDrink","5.9","토마토, 양상추, 쉑소스가 들어간 음료"),
-    DESSERTS("ShackDessert","4.9","토마토, 양상추, 쉑소스가 들어간 디저트");
-
+// 장바구니 값 저장
+public class CartItem {
     private final String name,price,description;
 
-    CartItem(String name,String price,String description){
+    public CartItem(String name,String price,String description){
         this.name = name;
         this.price = price;
         this.description = description;
